296ade3659be511420e8d00b3caca3faeca52bd3
Marc Andre Tanner <mat@brain-dump.org>
2008-04-03 23:22:38 +0200

    Cleanup config.mk file.

--- config.mk
+++ config.mk
@@ -1,15 +1,22 @@
-# fs version
-VERSION = $(shell date +%d.%m.%Y)
+# ciopfs version
+VERSION = 0.0.1
 
 # Customize below to fit your system
 
+CC = cc
 PREFIX = /usr/local
-#PREFIX = /usr
 MANPREFIX = ${PREFIX}/share/man
 
-CFLAGS += $(shell pkg-config --cflags fuse) -DFUSE_USE_VERSION=26 -DHAVE_LIBICUUC -DVERSION=\"${VERSION}\" -DNDEBUG -Os
-LDFLAGS += $(shell pkg-config --libs fuse) -lattr -lulockmgr -licuuc
+CFLAGS_FUSE  = $(shell pkg-config --cflags fuse) -DFUSE_USE_VERSION=26
+LDFLAGS_FUSE = $(shell pkg-config --libs fuse) -lulockmgr
+
+LDFLAGS_XATTR = -lattr
+
+# optional for unicode supprt
+CFLAGS_ICU  = -DHAVE_LIBICUUC
+LDFLAGS_ICU = -licuuc
+
+CFLAGS  += ${CFLAGS_FUSE}  ${CFLAGS_ICU} -DVERSION=\"${VERSION}\" -DNDEBUG -Os
+LDFLAGS += ${LDFLAGS_FUSE} ${LDFLAGS_ICU} ${LDFLAGS_XATTR}
 
 DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb -Wall
-
-CC = cc
